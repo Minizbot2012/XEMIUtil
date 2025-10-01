011ef7df2f50b37d103f3668d6992e942db99a1c
Mini <mini@minizbot.org>
2025-10-01 09:40:37 -0400

    SKSE Pre-AE is not fun

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,35 +1,40 @@
 #include <Config.h>
 #include <Hooks.h>
-#if defined(SKYRIM_AE) || defined(SKYRIM_SE)
+#include <Plugin.h>
+#ifdef SKYRIM_AE
 extern "C" DLLEXPORT auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v;
-    v.PluginVersion({ 1, 0, 0, 0 });
-    v.PluginName("XEMIUtil");
-    v.AuthorName("mini");
+    v.PluginVersion(MPL::Plugin::MAJOR);
+    v.PluginName(MPL::Plugin::PROJECT.data());
+    v.AuthorName("Mini");
     v.UsesAddressLibrary();
     v.UsesNoStructs();
     return v;
 }();
 
-#elif defined(SKYRIM_VR)
+#else
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
     a_info->infoVersion = SKSE::PluginInfo::kVersion;
-    a_info->name = "XEMIUtil";
-    a_info->version = 0x10000000;
+    a_info->name = MPL::Plugin::PROJECT.data();
+    a_info->version = MPL::Plugin::MAJOR;
     if (a_skse->IsEditor())
     {
         return false;
     }
     const auto ver = a_skse->RuntimeVersion();
+    #ifdef SKYRIMVR
     if (ver < SKSE::RUNTIME_VR_1_4_15)
+    #else
+    if(ver != SKSE::RUNTIME_SSE_1_5_97)
+    #endif
     {
         return false;
     }
     return true;
 };
-
 #endif
+
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
     SKSE::Init(a_skse);

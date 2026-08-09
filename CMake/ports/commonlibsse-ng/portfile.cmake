a9ad31aec650f27b990013fb5d7e05b2eaa4b27a
Mini <mini@minizbot.org>
2026-08-09 19:46:36 -0400

    Update portfile.cmake

--- CMake/ports/commonlibsse-ng/portfile.cmake
+++ CMake/ports/commonlibsse-ng/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/alandtse/CommonLibSSE-NG
-    REF 15ad90b31b2ca09cf477c7e1a43ff5ff05af120f
+    REF 5ae93ea9059aae23990ad7f2cbf3a2624d85c117
     HEAD_REF ng
 )
 
